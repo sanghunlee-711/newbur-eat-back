c0c982c9179fc836826c528a03eef689b55c5f62
cloudlee711 <cloudlee711@gmail.com>
2021-03-19 10:24:29 +0900

    Add: Install gateway - Figuring out Bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  //forRoot는 모듈의 루트를 잡아주기 위해서 import하는것
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    //code first , schema first중 code first로 사용
+    //https://docs.nestjs.com/graphql/quick-start#code-first
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
