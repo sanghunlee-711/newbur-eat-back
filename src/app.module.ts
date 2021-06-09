c8dc2292eb68da896dd0c364e05a86fadd813360
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-09 23:53:24 +0900

    [Add] Struct > entitiy, resolver 추가 - entity는 db 테이블의 모델 정의라고 보면 편함 - resolver는 해당 테이블의 관련된 쿼리 및 그 쿼리에 대한 로직을   정의하는곳 - module에서는 app.module.ts(root)에 제공 해주는 역할

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,10 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 //Restaurant클래스의 Resolver가 됨.
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
+  //Query가 리턴하고자 하는 타입을 리턴하는 함수가 되어야한다. (쿼리 데코레이터 안의 함수)
   @Query(() => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
