11aa4799ac1c140d737c001e982b6c456cab02e9
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-11 01:04:57 +0900

    [Add] class validator and pipeline - entity validation 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 
   if (module.hot) {
--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,6 +1,7 @@
 //dto: data transfer object
 
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
 
 // @InputType()
 //input 타입과 다르게 args타입은 분리하여 인자들을 선언하며 사용할 수 있음.
@@ -8,11 +9,18 @@ import { ArgsType, Field } from '@nestjs/graphql';
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field((type) => Boolean)
   isVegan: boolean;
+
   @Field((type) => String)
+  @IsString()
   address: string;
+
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
