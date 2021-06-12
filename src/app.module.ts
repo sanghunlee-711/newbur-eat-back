33ad219d63216fc6f4c545b920a124d2c0e6b695
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-13 01:56:29 +0900

    [Add] Postgresql 과 연결 - mysql이 아닌 postgresql과 연결 및 typeorm 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   //forRoot는 모듈의 루트를 잡아주기 위해서 import하는것
   imports: [
+    TypeOrmModule.forRoot({
+      //https://github.com/typeorm/typeorm 여기에 config 옵션들 있다.
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'cloudlee',
+      password: '1234', //postgres는 기본적으로 localhost로 호출하면 pw를 묻지 않는다
+      database: 'nuber-eats',
+      synchronize: true, //typeorm이d db를 연결할때 현재상태로 migration한다는 뜻임
+      logging: true, //무슨 일이 일어나는지 console에 나타냄
+    }),
     GraphQLModule.forRoot({
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       //true로 설정하면 메모리에 가지고 있고 파일로 안가지고 있음 query파일을
