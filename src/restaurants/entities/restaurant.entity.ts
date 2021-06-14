4c19813da9e48a063def105f43fdaa49508eefe2
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-14 22:01:39 +0900

    [Add] Mapped Type - dto와 entity에 공통으로 선언되어 있던 필드 코드들을 OmitType을 통해   Restaurant클래스(entitiy에 있는것)을 extends하여 중복 코드를 줄임 - @InputType 데코레이터의 isAbstract 옵션을 통해 ObjectType으로   명시되어있지만 InputType으로도 확장 가능할 수 있게만듦.

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,7 +1,10 @@
 //entity는 database의 모델을 생각하면 된다.
 
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@InputType({ isAbstract: true }) //스키마는 하나의 type을 가져야하는데 이렇게 isAbstract옵션을 이용함으로서 다른 곳에서 InputType으로서 사용할 수 있음.
+//InputType으로 쓴다는게 아니라 InputType으로도 extend시킨다는 뜻으로 이해하자!
 @ObjectType() //@ObjectType은 graphql이 자동으로 스키마를 빌드하기 위해 사용하는 Graphql decorator이다.
 @Entity() //@Entitysms TypeOrm이 db에 정의한 schema를 저장하게 해주기위해 사용하는 데코레이터이다.
 export class Restaurant {
@@ -12,21 +15,27 @@ export class Restaurant {
 
   @Field(() => String)
   @Column()
+  @IsString()
   name: string;
   //Nullabe 셋팅을 통해 ?연산자로 활용 가능
   @Field(() => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
+  @Length(5)
   ownerName: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -19,7 +19,7 @@ export class RestaurantResolver {
   //createRestaurant 에 일일이 arguments로서 restaurant.entity.ts에 있는 필드들을 필요한 것만 정의해서 Arg를 길게 가져 가도 됨
   // 그러나 inputType을 설정하여 객체를 전체로 넘겨서 쓸 수 도 있음
   async createRestaurant(
-    @Args() createRestaurantDto: CreateRestaurantDto,
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
   ): Promise<boolean> {
     try {
       await this.restaurantService.createRestaurant(createRestaurantDto);
