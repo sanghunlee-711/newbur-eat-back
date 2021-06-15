5a2fbb91e6c3212194d74523fa0abcbba5b3abf0
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-16 01:37:03 +0900

    [Add] hash password with bcrypt module

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,11 +1,13 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import {
   Field,
   InputType,
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
+import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 
 // type UserRole = 'client' | 'owenr' | 'delivery';
 
@@ -32,4 +34,14 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole }) // for DB
   @Field(() => UserRole) //for graphql
   role: UserRole;
+
+  @BeforeInsert() //before save in DB with save method in service.ts do this method, when users instance is made by create method
+  async hasPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
