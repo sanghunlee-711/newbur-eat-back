551285aaf1a7706f865d50272a2df8bd2861ddd4
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-26 22:02:29 +0900

    [Add] 메일 템플릿을 이용하여 보내는 방법 - 그냥 텍스트도 가능하지만 Mailgun의 template을 활용하여 커스터마이징도   가능

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,5 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailService } from './mail.service';
 import { MailModuleOptions } from './main.interface';
 
 @Module({})
@@ -12,8 +13,9 @@ export class MailModule {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
+        MailService,
       ],
-      exports: [],
+      exports: [MailService],
     };
   }
 }
