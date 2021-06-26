f5444eea41184fd03039365f4e845ae6cf899e94
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-26 22:28:49 +0900

    [Add] 유저 가입, 정보수정 경우 메일 전송

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,9 +1,10 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { MailService } from './mail.service';
 import { MailModuleOptions } from './main.interface';
 
 @Module({})
+@Global()
 export class MailModule {
   static forRoot(options: MailModuleOptions): DynamicModule {
     return {
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,29 +2,32 @@ import { Inject, Injectable } from '@nestjs/common';
 import * as FormData from 'form-data';
 import got from 'got';
 import { CONFIG_OPTIONS } from '../common/common.constants';
-import { MailModuleOptions } from './main.interface';
+import { EmailVars, MailModuleOptions } from './main.interface';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail('testing', 'test');
-  }
+  ) {}
 
-  private async sendEmail(subject: string, template: string) {
+  private async sendEmail(
+    subject: string,
+    to: string,
+    template: string,
+    emailVars: EmailVars[],
+  ) {
     const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
-    form.append('to', `cloudlee711@gmail.com`);
+    form.append('from', `Hoon From Test PJT <mailgun@${this.options.domain}>`);
+    form.append('to', `${to}`);
     form.append('subject', subject);
     // form.append('text', content);
     form.append('template', template);
-    form.append('v:code', 'testtest');
-    form.append('v:username', 'sanghun');
+    // form.append('v:code', 'testtest');
+    // form.append('v:username', 'sanghun');
+    emailVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
 
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      {
+    try {
+      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         method: 'POST',
         headers: {
           Authorization: `Basic ${Buffer.from(
@@ -33,9 +36,16 @@ export class MailService {
         },
 
         body: form,
-      },
-    );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    console.log('@@@@@', response.body);
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Verify Your Email', 'cloudlee711@gmail.com', 'test', [
+      { key: 'code', value: code },
+      { key: 'username', value: email },
+    ]);
   }
 }
