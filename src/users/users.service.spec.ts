3bfd26f007a67f6018cda57b544f553ffdf3284f
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-27 17:06:23 +0900

    [Add] User > CreateAccount Method Test Done - test args type with expect.any(String) - Helpful to set mocking return value before calling test method

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -8,11 +8,11 @@ import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
 
 //users repository를 가짜로 만듦
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 
 const mockJwtService = {
   sign: jest.fn(),
@@ -32,6 +32,9 @@ describe('User Service', () => {
 
   let service: UsersService;
   let usersRepository: MockRepository<User>;
+  let verificationRepository: MockRepository<Verification>;
+  let mailService: MailService;
+
   beforeAll(async () => {
     //1.테스트 하고 싶은 모듈을 만든다.
     const module = await Test.createTestingModule({
@@ -41,12 +44,12 @@ describe('User Service', () => {
         UsersService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
           //TypeORM에서 repository를 불러오는게 아닌 MockRepository를 만들어서 제공할 것임.
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -61,7 +64,9 @@ describe('User Service', () => {
 
     //2.만든 모듈에서 service만을 가져오는 방법
     service = module.get<UsersService>(UsersService);
+    mailService = module.get<MailService>(MailService);
     usersRepository = module.get(getRepositoryToken(User));
+    verificationRepository = module.get(getRepositoryToken(Verification));
   });
 
   it('Should be defined', () => {
@@ -69,6 +74,12 @@ describe('User Service', () => {
   });
 
   describe('createAccount', () => {
+    const createAccountArgs = {
+      email: '',
+      password: '',
+      role: 0,
+    };
+
     it('Should fail if user exists', async () => {
       //유저서비스의 createAccount 메서드 중 아래의 코드를 테스트 해봄
       // 전체 반응을 테스트하는 것은 E2E테스트에 가깝고 unitTest의 경우 각 줄의 코드가
@@ -84,21 +95,58 @@ describe('User Service', () => {
         //위의 코드부분을 목킹하여 가짜 return 값을 설정해주는 과정
         //jest와 Mock을 사용하여 실제 user.service.ts에 있는 dependencies에 포함된 함수의 반환값을 속일 수 있다.
         id: 1,
-        email: 'testHello@test.com',
+        email: '',
       });
 
-      const result = await service.createAccount({
-        email: '',
-        password: '',
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
 
       expect(result).toMatchObject({
         ok: false,
         error: '중복된 이메일로 이미 유저가 존재합니다.',
       });
     });
+
+    it('shold Create New User', async () => {
+      //Set Repositories Return value
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+
+      verificationRepository.create.mockReturnValue({
+        user: createAccountArgs,
+      });
+      verificationRepository.save.mockResolvedValue({
+        code: 'code',
+      });
+      //End Setting
+
+      const result = await service.createAccount(createAccountArgs);
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verificationRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.save).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        //어떤 타입으로 호출 되는지를 확인하는 방법 여기서는 user.email, verification.code가 string임
+        expect.any(String),
+        expect.any(String),
+      );
+
+      expect(result).toEqual({ ok: true });
+    });
   });
+
   it.todo('login');
   it.todo('findByID');
   it.todo('editProfile');
