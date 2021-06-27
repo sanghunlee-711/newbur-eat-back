5c86a95912d0f8155042e84ce362090e7757a00a
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-27 12:24:28 +0900

    [Add] Service 정의하는 unitTest - mocking을 통해 테스트하고자하는 서비스 이외의 것들을 임의로 부여해줌 - jest.fn()을 통해 메서드를 대체함

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,26 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
+
+//users repository를 가짜로 만듦
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('User Service', () => {
   //유저 서비스를 테스트하게 됨.
@@ -9,7 +30,27 @@ describe('User Service', () => {
     //1.테스트 하고 싶은 모듈을 만든다.
     const module = await Test.createTestingModule({
       //모듈로 Imports하고 싶은것들이 포함된다.
-      providers: [UsersService],
+      providers: [
+        //constructor에서 받아오는 것들을 mocking하는 과정
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+          //TypeORM에서 repository를 불러오는게 아닌 MockRepository를 만들어서 제공할 것임.
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
 
     //2.만든 모듈에서 service만을 가져오는 방법
