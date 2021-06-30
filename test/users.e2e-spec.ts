c7ee9f06bcf684b744aa75826d485174c611ce7d
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-01 00:47:51 +0900

    [Add] login E2E test case - createAccount 와 거의 유사한 방식..

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -10,10 +10,14 @@ jest.mock('got', () => {
   };
 });
 const GRAPHQL_ENDPOINT = '/graphql';
+const testUser = {
+  EMAIL: 'test@test.com',
+  PASSWORD: '1234',
+};
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
-
+  let jwtToken: string;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -29,16 +33,14 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'test@test.com';
-
     it('should create account', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `mutation{
             createAccount(input:{
-              email: "${EMAIL}",
-              password: "1234",
+              email: "${testUser.EMAIL}",
+              password: "${testUser.PASSWORD}",
               role:Owner
             }){
               ok
@@ -57,28 +59,99 @@ describe('UserModule (e2e)', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
-          query: `mutation{
+          query: `
+          mutation{
             createAccount(input:{
-              email: "${EMAIL}",
-              password: "1234",
+              email: "${testUser.EMAIL}",
+              password: "${testUser.PASSWORD}",
               role:Owner
             }){
               ok
               error
             }
-          }`,
+          }
+          `,
         })
         .expect(200)
         .expect((res) => {
-          console.log(res.body.data);
-          expect(res.body.data.createAccount.ok).toBe(false);
-          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+          const {
+            body: {
+              data: { createAccount },
+            },
+          } = res;
+          expect(createAccount.ok).toBe(false);
+          expect(createAccount.error).toEqual(expect.any(String));
           //toBe는 완전 똑같아야 하고 toEqual의 경우 위와같이 타입으로 유사정도 확인 가능함 ㅇㅇ
         });
     });
   });
+
+  describe('login', () => {
+    it('should log in with correct credentials(token)', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            login(input:{
+              email: "${testUser.EMAIL}",
+              password:"${testUser.PASSWORD}",
+            }){
+              ok
+              error
+              token
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+
+          expect(login.ok).toBeTruthy();
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credential(code)', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation{
+          login(input:{
+            email: "${testUser.EMAIL}",
+            password:"${testUser.PASSWORD}-testError",
+          }){
+            ok
+            error
+            token
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+
+          expect(login.ok).toBeFalsy();
+          expect(login.error).toBe('Wrong Password');
+          expect(login.token).toEqual(null);
+        });
+    });
+  });
+
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
