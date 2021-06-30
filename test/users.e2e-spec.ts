a0d9c6ffcca767e7bcf08ad41b165935b9f5952d
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-01 00:28:27 +0900

    [Add] createAccount E2E test case

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,11 @@ import * as request from 'supertest';
 import { getConnection } from 'typeorm';
 import { AppModule } from '../src/app.module';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
@@ -45,6 +50,30 @@ describe('UserModule (e2e)', () => {
         .expect((res) => {
           expect(res.body.data.createAccount.ok).toBeTruthy();
           expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if create exist', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+            createAccount(input:{
+              email: "${EMAIL}",
+              password: "1234",
+              role:Owner
+            }){
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          console.log(res.body.data);
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+          //toBe는 완전 똑같아야 하고 toEqual의 경우 위와같이 타입으로 유사정도 확인 가능함 ㅇㅇ
         });
     });
   });
