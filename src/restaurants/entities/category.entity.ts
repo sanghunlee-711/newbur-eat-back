61ade6ce46e547abfbd62aed4058227a9c2f7954
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-03 16:08:47 +0900

    [Add] createRestaurant method

--- src/restaurants/entities/category.entity.ts
+++ src/restaurants/entities/category.entity.ts
@@ -9,18 +9,23 @@ import { Restaurant } from './restaurant.entity';
 @Entity() //@Entity는 TypeOrm이 db에 정의한 schema를 저장하게 해주기위해 사용하는 데코레이터이다.
 export class Category extends CoreEntity {
   @Field(() => String)
-  @Column()
+  @Column({ unique: true })
   @IsString()
   @Length(5)
   name: string;
 
-  @Field(() => String)
-  @Column()
+  @Field(() => String, { nullable: true })
+  @Column({ nullable: true })
   @IsString()
   coverImg: string;
 
+  @Field(() => String)
+  @Column({ unique: true })
+  @IsString()
+  slug: string;
+
   //하나의 카테고리는 많은 레스토랑을 가짐, 레스토랑은 하나의 카테고리만 가짐
-  @Field(() => [Restaurant])
+  @Field(() => [Restaurant], { nullable: true })
   @OneToMany(() => Restaurant, (restaurant) => restaurant.category) //TYPEORM에서 관계정의시 inverse또한 정의를 해줘야함.
   restaurants: Restaurant[];
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import {
+  CreateRestaurantInput,
+  CreateRestaurantOutPut,
+} from './dtos/create-restaurant.dto';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -10,6 +14,8 @@ export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Category)
+    private readonly categories: Repository<Category>,
   ) {}
 
   getAll(): Promise<Restaurant[]> {
@@ -18,16 +24,33 @@ export class RestaurantService {
     return this.restaurants.find();
   }
 
-  createRestaurant(
-    createRestaurantDto: CreateRestaurantDto,
-  ): Promise<Restaurant> {
-    //create 와 save의 차이를 보자
-    const newRestaurant = this.restaurants.create(createRestaurantDto); // create는 instance를 만들어 줌 -> DB를 실제로 건들지는 않는다는 말.
-    //DB에 실제로 저장하고 싶으면 save를 사용하면 된다.
-    return this.restaurants.save(newRestaurant);
-  }
+  async createRestaurant(
+    owner: User,
+    createRestaurantInput: CreateRestaurantInput,
+  ): Promise<CreateRestaurantOutPut> {
+    try {
+      const newRestaurant = this.restaurants.create(createRestaurantInput);
+      newRestaurant.owner = owner;
 
-  updateRestaurant({ id, data }: UpdateRestaurantDto) {
-    return this.restaurants.update(id, { ...data });
+      const categoryName = createRestaurantInput.categoryName
+        .trim()
+        .toLowerCase();
+      const categorySlug = categoryName.replace(/ /g, '-');
+      let category = await this.categories.findOne({ slug: categorySlug });
+
+      if (!category) {
+        category = await this.categories.save(
+          this.categories.create({ slug: categorySlug, name: categoryName }),
+        );
+      }
+
+      newRestaurant.category = category;
+      await this.restaurants.save(newRestaurant);
+      return {
+        ok: true,
+      };
+    } catch {
+      return { ok: false, error: 'Could not create restaurant' };
+    }
   }
 }
