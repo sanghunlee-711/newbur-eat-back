96bee9b68c8cd016ab2058058b5359500cedbfd6
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-03 20:18:02 +0900

    [Add] 주석 추가

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -8,14 +8,18 @@ import { AllowedRoles } from './role.decorator';
 //authorization => can you access this ?
 
 @Injectable()
+//CanActivate는 true또는 false밖에 리턴 못함 :)
 export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext) {
+    //셋팅된 메타데이터를 가져오기 위해 Reflector를 사용
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
     );
+    console.log('In AUTH GUARD', roles);
+
     if (!roles) {
       return true;
     }
