f283acd18eddd3c9f643473a4c9b6290b121ecb1
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-06 23:52:59 +0900

    [Add] order entity > graphql setting

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,4 +1,10 @@
-import { InputType, ObjectType } from '@nestjs/graphql';
+import {
+  Field,
+  Float,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
@@ -12,21 +18,28 @@ export enum OrderStatus {
   Delivered = 'Delivered',
 }
 
+registerEnumType(OrderStatus, { name: 'OrderStatus' });
 @InputType('OrderInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Order extends CoreEntity {
   //1.ts , 2.gq , 3.typeorm
 
+  @Field((type) => User)
   customer: User;
 
+  @Field((type) => User, { nullable: true })
   driver: User;
 
+  @Field((type) => Restaurant)
   restaurant: Restaurant;
 
+  @Field(() => [Dish])
   dishes: Dish[];
 
+  @Field(() => Float)
   total: number;
 
+  @Field(() => OrderStatus)
   status: OrderStatus;
 }
