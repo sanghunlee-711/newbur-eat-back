241d17b004476ecf6d0b0d7ea3674a492749ff02
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-15 01:27:23 +0900

    [Add] Theory of subscription

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -56,18 +56,32 @@ export class OrderResolver {
   }
 
   @Mutation(() => Boolean)
-  potatoReady() {
+  async potatoReady(@Args('potatoId') potatoId: number) {
     //potatoReady는 asyncIterator(hotPotatos)를 발생시키는 작업을 한다.
     //트리거 이름과 publish이름은 같아야함(hotPotato)
     //publish의 payload는 객체여야하고 Mutation function과 이름이 같으면 됨 readyPotato
-    this.pubSub.publish('hotPotato', { readyPotato: 'potato is ready :)' });
+    await this.pubSub.publish('hotPotato', {
+      readyPotato: potatoId,
+    });
     return true;
   }
 
-  @Subscription(() => String)
+  @Subscription(() => String, {
+    //현재 listening하는 사용자가 update알림을 받아야할지 말지를 결정해주는 함수가 filter
+    filter: ({ readyPotato }, { potatoId }) => {
+      // filter메서드에서는 3가지 인자를 받을 수 있는데 payload, variables, context임
+      // filtering을 위해서 pubSub.publish로 받는 인자가 payload,
+      // subScription 데코레이터가 달려있는 메서드에서 args로 받는 것이 variables임
+      // 그 두가지를 비교해서 filtering을 하게 됨
+      return readyPotato === potatoId;
+    },
+    //사용자가 받는 알림의 형태를 바꿔주는 함수가 resolve
+    resolve: ({ readyPotato }) =>
+      `Your potato with the id ${readyPotato} is Ready!`,
+  })
   @Role(['Any'])
-  readyPotato(@AuthUser() user: User) {
-    console.log(user);
+  readyPotato(@Args('potatoId') potatoId: number) {
+    //특정 mutation을 리스닝하기 위해 asyncIterator를 사용
     return this.pubSub.asyncIterator('hotPotato');
   }
 }
