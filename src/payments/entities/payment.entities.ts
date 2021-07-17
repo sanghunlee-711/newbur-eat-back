b192ba1f7589bccdd50fd41cba02affc04f7d725
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-17 12:42:33 +0900

    [Add] CreatePayment resovler and service

--- src/payments/entities/payment.entities.ts
+++ src/payments/entities/payment.entities.ts
@@ -8,25 +8,27 @@ import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 @ObjectType()
 @Entity()
 export class Payment extends CoreEntity {
-  @Field(() => Int)
+  @Field(() => String)
   @Column()
-  transactionId: number;
+  transactionId: string;
 
   @Field(() => User)
   @ManyToOne(() => User, (user) => user.payments, {
     nullable: true,
   })
-  user?: User;
+  user: User;
 
   @RelationId((payment: Payment) => payment.user)
   userId: number;
 
   // 레스토랑에서 payment로 접근할 일이 없기 때문에 retaurant entity에 따로 restaurant를 선언해주지 않는다.
   // 그리고 reverse relation도 따로 선언해주지 않는다.
+  // 공식문서를 보면 설명잘되어있음 oneTomany는 이렇게 선언하지 못함 ㅎㅅㅎ
   @Field(() => Restaurant)
   @ManyToOne(() => Restaurant)
   restaurant: Restaurant;
 
   @RelationId((payment: Payment) => payment.restaurant)
+  @Field(() => Int)
   restaurantId: number;
 }
---
+++ src/payments/payments.resolver.ts
@@ -0,0 +1,24 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth.user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
+import { Payment } from './entities/payment.entities';
+import { PaymentService } from './payments.service';
+
+@Resolver((of) => Payment)
+export class PaymentResolver {
+  constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation(() => CreatePaymentOutput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ): Promise<CreatePaymentOutput> {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
+}
